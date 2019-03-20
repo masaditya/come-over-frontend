50b026f7b2b9115eb089cfee7cdfa9054e730711
adit <aeka489@gmail.com>
2019-03-20 11:06:01 +0700

    fixing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,37 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from "./routes/app-routing.module";
 import { AppComponent } from "./app.component";
-import { LandingPagesComponent } from './pages/landing-pages/landing-pages.component';
-import { BrowseAllPagesComponent } from './pages/browse-all-pages/browse-all-pages.component';
-import { DetailEventPagesComponent } from './pages/detail-event-pages/detail-event-pages.component';
-import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
-import { CreateEventPagesComponent } from './pages/create-event-pages/create-event-pages.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CardEventComponent } from './components/card-event/card-event.component';
-import { ModalRegisterEventComponent } from './components/modal-register-event/modal-register-event.component';
-import { ManageEventPagesComponent } from './pages/manage-event-pages/manage-event-pages.component';
-import { OrganizerProfilePagesComponent } from './pages/organizer-profile-pages/organizer-profile-pages.component';
-import { UserProfilePagesComponent } from './pages/user-profile-pages/user-profile-pages.component';
+import { LandingPagesComponent } from "./pages/landing-pages/landing-pages.component";
+import { BrowseAllPagesComponent } from "./pages/browse-all-pages/browse-all-pages.component";
+import { DetailEventPagesComponent } from "./pages/detail-event-pages/detail-event-pages.component";
+import { LoginPagesComponent } from "./pages/login-pages/login-pages.component";
+import { CreateEventPagesComponent } from "./pages/create-event-pages/create-event-pages.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { CardEventComponent } from "./components/card-event/card-event.component";
+import { ModalRegisterEventComponent } from "./components/modal-register-event/modal-register-event.component";
+import { ManageEventPagesComponent } from "./pages/manage-event-pages/manage-event-pages.component";
+import { OrganizerProfilePagesComponent } from "./pages/organizer-profile-pages/organizer-profile-pages.component";
+import { UserProfilePagesComponent } from "./pages/user-profile-pages/user-profile-pages.component";
 
 @NgModule({
-  declarations: [AppComponent, LandingPagesComponent, BrowseAllPagesComponent, DetailEventPagesComponent, LoginPagesComponent, CreateEventPagesComponent, HeaderComponent, FooterComponent, CardEventComponent, ModalRegisterEventComponent, ManageEventPagesComponent, OrganizerProfilePagesComponent, UserProfilePagesComponent],
+  declarations: [
+    AppComponent,
+    LandingPagesComponent,
+    BrowseAllPagesComponent,
+    DetailEventPagesComponent,
+    LoginPagesComponent,
+    CreateEventPagesComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardEventComponent,
+    ModalRegisterEventComponent,
+    ManageEventPagesComponent,
+    OrganizerProfilePagesComponent,
+    UserProfilePagesComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
