5ca40b2463eca3f5330d154d8fde50ed1820b57f
adit <aeka489@gmail.com>
2019-04-14 00:40:07 +0700

    fix details

--- src/app/pages/detail-event-pages/detail-event-pages.component.ts
+++ src/app/pages/detail-event-pages/detail-event-pages.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-detail-event-pages',
-  templateUrl: './detail-event-pages.component.html',
-  styleUrls: ['./detail-event-pages.component.css']
+  selector: "app-detail-event-pages",
+  templateUrl: "./detail-event-pages.component.html",
+  styleUrls: ["./detail-event-pages.component.css"]
 })
 export class DetailEventPagesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  cards: number[] = [1, 2, 4, 5, 6, 3];
 }
