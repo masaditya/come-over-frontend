ce9bc91a864dd76a614dc03dc5fbe04721630e36
adit <aeka489@gmail.com>
2019-05-12 18:01:47 +0700

    deployment

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/come-over-fronten'));
+app.use(express.static(__dirname + '/dist/come-over'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/come-over-fronten/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/come-over/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
